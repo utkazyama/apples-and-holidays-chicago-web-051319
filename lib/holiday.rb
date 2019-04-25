c8ca0c80d1c141b2946cdda14ccb6bb2ddde1c8a
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-25 03:58:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,9 +54,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
-    if supply.include? ("BBQ")
+      holiday if supply.include? ("BBQ")
     end
-  end
   end.flatten
 end
 
